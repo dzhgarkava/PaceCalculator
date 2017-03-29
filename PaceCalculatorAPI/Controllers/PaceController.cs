8a9fb23864ae3aa0a9720ca25770195133703a4a
George Dzhgarkava <george.dzhgarkava@capitalontap.com>
2017-03-29 15:51:59 +0100

    Added PaceController

---
+++ PaceCalculatorAPI/Controllers/PaceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PaceCalculatorAPI.Models;
+
+namespace PaceCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PaceController : Controller
+    {
+        // GET api/pace
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return BadRequest("Pace is empty");
+        }
+
+        // GET api/pace/3-45
+        [HttpGet("{pace}")]
+        public IActionResult Get(string pace)
+        {
+            if (string.IsNullOrEmpty(pace)) return BadRequest("Pace is incorrect. Format mm-ss");
+
+            var input = pace.Split('-');
+            var p = new Pace(Convert.ToInt32(input[0]), Convert.ToInt32(input[1]));
+
+            var paces = new Dictionary<string, string>
+            {
+                {"half", p.GetFinishTime(21.097)},
+                {"marathon", p.GetFinishTime(42.195)}
+            };
+
+            for (var i = 1; i < 43; i++)
+            {
+                paces.Add($"{i}", p.GetFinishTime(i));
+            }
+
+            return new ObjectResult(paces);
+        }
+    }
+}
---
+++ PaceCalculatorAPI/Models/Pace.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaceCalculatorAPI.Models
+{
+    public class Pace
+    {
+        public Pace(int minutes, int seconds)
+        {
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        private int Minutes { get; }
+        private int Seconds { get; }
+
+        public override string ToString()
+        {
+            var min = Minutes < 10 ? $"0{Minutes}" : $"{Minutes}";
+            var sec = Seconds < 10 ? $"0{Seconds}" : $"{Seconds}";
+            return $"{min}:{sec} min/km";
+        }
+
+        public string GetFinishTime(double distance)
+        {
+            var secPerKm = Minutes * 60 + Seconds;
+            var totalSec = distance * secPerKm;
+
+            var s = (int)totalSec;
+            var finishHours = s / 3600;
+            s = s % 3600;
+            var finishMin = s / 60;
+            s = s % 60;
+            var finishSec = s;
+
+            var hour = finishHours < 10 ? $"0{finishHours}" : $"{finishHours}";
+            var min = finishMin < 10 ? $"0{finishMin}" : $"{finishMin}";
+            var sec = finishSec < 10 ? $"0{finishSec}" : $"{finishSec}";
+            return $"{hour}:{min}:{sec}";
+        }
+    }
+}
